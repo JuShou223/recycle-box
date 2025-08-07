bb5acc83706aaef759e61b5e72ba03baf509f8b0
徐一鸣 <yiming.xu@nascent.cn>
2025-08-07 12:37:06 +0800

    feat: 添加图拍呢

--- src/app.config.js
+++ src/app.config.js
@@ -2,13 +2,13 @@ export default defineAppConfig({
   pages: [
     "pages/index/index",
     "pages/recycle/index",
-    "pages/points/index", 
+    "pages/points/index",
     "pages/profile/index",
     "pages/messages/index",
     "pages/scan/index",
     "pages/map/index",
     "pages/exchange/index",
-    "pages/ranking/index"
+    "pages/ranking/index",
   ],
   tabBar: {
     color: "#666666",
@@ -20,27 +20,27 @@ export default defineAppConfig({
         pagePath: "pages/index/index",
         text: "首页",
         iconPath: "assets/icons/home.png",
-        selectedIconPath: "assets/icons/home-active.png"
+        selectedIconPath: "assets/icons/home-active.png",
       },
       {
-        pagePath: "pages/recycle/index", 
+        pagePath: "pages/recycle/index",
         text: "回收",
         iconPath: "assets/icons/recycle.png",
-        selectedIconPath: "assets/icons/recycle-active.png"
+        selectedIconPath: "assets/icons/recycle-active.png",
       },
       {
         pagePath: "pages/points/index",
         text: "积分",
-        iconPath: "assets/icons/points.png", 
-        selectedIconPath: "assets/icons/points-active.png"
+        iconPath: "assets/icons/points.png",
+        selectedIconPath: "assets/icons/points-active.png",
       },
       {
         pagePath: "pages/profile/index",
         text: "我的",
         iconPath: "assets/icons/profile.png",
-        selectedIconPath: "assets/icons/profile-active.png"
-      }
-    ]
+        selectedIconPath: "assets/icons/profile-active.png",
+      },
+    ],
   },
   window: {
     backgroundTextStyle: "light",
